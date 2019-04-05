94b3779605f5533f691e625e3165ed5399ffff80
Paul Schreiber <paulschreiber@gmail.com>
2019-04-05 10:42:56 -0400

    add VIP Go CLI utility

---
+++ gocli/provision.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+# PACKAGE INSTALLATION
+#
+# Build a bash array to pass all of the packages we want to install to a single
+# apt-get command. This avoids doing all the leg work each time a package is
+# set to install. It also allows us to easily comment out or add single
+# packages. We set the array as empty to begin with so that we can append
+# individual packages to it as required.
+apt_package_install_list=(
+  libsecret-1-dev
+)
+
+# Build a bash array to pass all of the packages we want to install to a single
+# npm command. This avoids doing all the leg work each time a package is
+# set to install. It also allows us to easily comment out or add single
+# packages. We set the array as empty to begin with so that we can append
+# individual packages to it as required.
+npm_package_install_list=(
+  @automattic/vip
+)
+
+### FUNCTIONS
+
+network_detection() {
+  # Network Detection
+  #
+  # Make an HTTP request to google.com to determine if outside access is available
+  # to us. If 3 attempts with a timeout of 5 seconds are not successful, then we'll
+  # skip a few things further in provisioning rather than create a bunch of errors.
+  if [[ "$(wget --tries=3 --timeout=5 --spider --recursive --level=2 https://launchpad.net/ 2>&1 | grep 'connected')" ]]; then
+    echo "Network connection detected..."
+    ping_result="Connected"
+  else
+    echo "Network connection not detected. Unable to reach google.com..."
+    ping_result="Not Connected"
+  fi
+}
+
+network_check() {
+  network_detection
+  if [[ ! "$ping_result" == "Connected" ]]; then
+    echo -e "\nNo network connection available, skipping package installation"
+    exit 0
+  fi
+}
+
+package_install() {
+  if [[ ${#apt_package_install_list[@]} = 0 ]]; then
+    echo -e "No apt packages to install.\n"
+  else
+
+    # Update all of the package references before installing anything
+    echo "Running apt-get update..."
+    apt-get -y update
+
+    # Install required packages
+    echo "Installing apt-get packages..."
+    apt-get -y install ${apt_package_install_list[@]}
+
+    # Remove unnecessary packages
+    echo "Removing unnecessary packages..."
+    apt-get autoremove -y
+
+    # Clean up apt caches
+    apt-get clean
+  fi
+
+
+  if [[ ${#npm_package_install_list[@]} = 0 ]]; then
+    echo -e "No npm packages to install.\n"
+  else
+
+		npm install -g ${apt_package_install_list[@]}
+  fi
+}
+
+network_check
+package_install
